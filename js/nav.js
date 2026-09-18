37924708cf1e393898e5f61a1368b95e56fa4f92
mrbeanmol-hue <mrbeanmol@gmail.com>
2026-09-18 22:47:09 +0200

    Add clean URLs and loading fixes

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,36 @@
-let currentPage = window.location.pathname.split("/").pop();
-
-if (currentPage === "") {
-    currentPage = "index.html";
-}
+const currentPath = window.location.pathname.toLowerCase();
 
 const navigationLinks = document.querySelectorAll(".site-navigation a");
+
 navigationLinks.forEach(function (link) {
-    const linkPage = link.getAttribute("href");
-    if (linkPage === currentPage) {
-        link.classList.add("active-page");
-    }
+    link.classList.remove("active-page");
 });
+
+
+if (currentPath.includes("/gallery/")) {
+    document
+        .querySelector('a[href*="gallery/"]')
+        .classList.add("active-page");
+}
+
+
+else if (currentPath.includes("/about/")) {
+    document
+        .querySelector('a[href*="about/"]')
+        .classList.add("active-page");
+}
+
+
+else if (currentPath.includes("/contact/")) {
+    document
+        .querySelector('a[href*="contact/"]')
+        .classList.add("active-page");
+}
+
+else {
+    const homeLink = document.querySelector(".site-navigation a");
+
+    if (homeLink) {
+        homeLink.classList.add("active-page");
+    }
+}
